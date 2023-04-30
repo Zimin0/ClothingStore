1f5afc2558da773ff1b8bbfd0d4dafbc27d11d1f
Zimin0 <nikzim2004@gmail.com>
2023-04-30 14:57:34 +0300

    Deleting product_detail

--- pages/views.py
+++ pages/views.py
@@ -53,13 +53,3 @@ def ex_product(request, prod_id):
                'cart_product_form': cart_product_form}
     context['product'] = Product.objects.get(id=prod_id)
     return render(request, 'pages/product.html', context)
-
-
-def product_detail(request, id, slug):
-    product = get_object_or_404(Product,
-                                id=id,
-                                slug=slug,
-                                available=True)
-    cart_product_form = CartAddProductForm()
-    return render(request, 'shop/product/detail.html', {'product': product,
-                                                        'cart_product_form': cart_product_form})
