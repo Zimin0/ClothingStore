95d8fd5903fc2a7a059168086635464a3895de97
Zimin0 <you@example.com>
2023-05-18 16:50:07 +0300

    old test login view deleted

--- users/views.py
+++ users/views.py
@@ -27,25 +27,6 @@ def register(request):
     return render(request, 'registration/registration.html', {'user_form': user_form})
 
 
-### Удалить !!!
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
-    return render(request, 'registration/login.html', {'form': form})
-
 @login_required
 def edit(request):
     # Лучше перенести валидацию в clean_<field_name> в forms.py
